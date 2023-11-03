503734edd6c6fd1ee0944e5cf6af96a268b272be
j-seibel <107728907+j-seibel@users.noreply.github.com>
2023-11-03 13:09:01 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace GithubActionsLab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_Valid_Seibel()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_Invalid_Seibel()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_Null_Seibel()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -32,7 +32,7 @@ namespace GithubActionsLab
 
         [Test]
 
-        public void Subtract_Valid()
+        public void Subtract_Valid_Seibel()
         {
             Assert.AreEqual(-1, Program.Subtract("1", "2"));
             Assert.AreEqual(1, Program.Subtract("3", "2"));
@@ -40,7 +40,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Subtract_Invalid()
+        public void Subtract_Invalid_Seibel()
         {
             Assert.Throws<FormatException>(() => Program.Subtract("1", "a"));
             Assert.Throws<FormatException>(() => Program.Subtract("a", "1"));
@@ -48,7 +48,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Subtract_Null()
+        public void Subtract_Null_Seibel()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Subtract("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Subtract(null, "1"));
@@ -56,7 +56,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Mult_Valid()
+        public void Mult_Valid_Seibel()
         {
             Assert.AreEqual(2, Program.Multiply("1", "2"));
             Assert.AreEqual(6, Program.Multiply("3", "2"));
@@ -64,7 +64,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Mult_Invalid()
+        public void Mult_Invalid_Seibel()
         {
             Assert.Throws<FormatException>(() => Program.Multiply("1", "a"));
             Assert.Throws<FormatException>(() => Program.Multiply("a", "1"));
@@ -72,7 +72,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Mult_Null()
+        public void Mult_Null_Seibel()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Multiply("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, "1"));
@@ -80,7 +80,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Div_Valid()
+        public void Div_Valid_Seibel()
         {
             Assert.AreEqual(2, Program.Divide("2", "1"));
             Assert.AreEqual(1, Program.Divide("3", "3"));
@@ -88,7 +88,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Div_Invalid()
+        public void Div_Invalid_Seibel()
         {
             Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
             Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
@@ -96,7 +96,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Div_Null()
+        public void Div_Null_Seibel()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
@@ -105,15 +105,15 @@ namespace GithubActionsLab
 
 
         [Test]
-        public void Pow_Valid()
+        public void Pow_Valid_Seibel()
         {
             Assert.AreEqual(1, Program.Power("1", "2"));
             Assert.AreEqual(9, Program.Power("3", "2"));
-            Assert.AreEqual(5, Program.Power("5", "2"));
+            Assert.AreEqual(25, Program.Power("5", "2"));
         }
 
         [Test]
-        public void Pow_Invalid()
+        public void Pow_Invalid_Seibel()
         {
             Assert.Throws<FormatException>(() => Program.Power("1", "a"));
             Assert.Throws<FormatException>(() => Program.Power  ("a", "1"));
@@ -121,7 +121,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Pow_Null()
+        public void Pow_Null_Seibel()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Power("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Power    (null, "1"));
